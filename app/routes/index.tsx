24a6e1d6830f706717d1c963495bcc69e18056d4
pilot Tobi <tobi@tangleswap.exchange>
2022-04-11 13:43:12 +0100

    add index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,16 @@
 import React from "react"
+import styled from "styled-components"
 
 const index = () => {
-  return <div>index</div>
+  return <Body>Coming soon</Body>
 }
 
+const Body = styled.div`
+  background: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 32px;
+  color: #fff;
+`
 export default index
